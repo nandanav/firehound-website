7a71dbc47b115daa185d1d0fe4cfeaa5d7402eca
Abhinav Vemulapalli <nandan.av11@gmail.com>
2021-10-09 19:25:22 -0600

    Added files to deploy on Heroku

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module cs_capstone_website
 
 go 1.16
-
-require github.com/carlmjohnson/gateway v1.20.7
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"flag"
+	"os"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/carlmjohnson/gateway"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,18 +17,14 @@ func journalViewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
-	flag.Parse()
-	listener := gateway.ListenAndServe
-	portStr := "n/a"
-	if *port != -1 {
-		portStr = fmt.Sprintf(":%d", *port)
-		listener = http.ListenAndServe
-		// http.Handle("/", http.FileServer(http.Dir("./public")))
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
 	}
 
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/journal/", journalViewHandler)
 
-	log.Fatal(listener(portStr, nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
